d57fda29bfeed860db7c27a3ed27fc8d02250609
Wüst Alexander (wuestal2) <wuestal2@students.zhaw.ch>
2023-11-27 20:57:07 +0100

    add Plant class

---
+++ PlantGenius.GUI/Plant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantGenius.GUI
+{
+    public class Plant
+    {
+        public Plant(string PlantName, DateTime WaterLastTime, int WaterRequirement)
+        {
+            this.PlantName = PlantName;
+            this.WaterLastTime = WaterLastTime;
+            this.WaterRequirement = WaterRequirement;
+        }
+
+        public string PlantName { get; set; }
+        public DateTime WaterLastTime { get; set; }
+        public int WaterRequirement { get; set; }
+        // public Room Room { get; set; }
+    }
+}
