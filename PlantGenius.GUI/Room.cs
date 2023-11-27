2e8a55171c437156008fa471cfce0d84bba92c0d
Perrine Rotzetter <rotzeper@students.zhaw.ch>
2023-11-27 20:57:46 +0100

    Added Room class

---
+++ PlantGenius.GUI/Room.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//TODO: Should the room have a list with plants?
+
+namespace PlantGenius.GUI
+{
+    /// <summary>
+    /// A room has a name. If an apartment or a house has different floors, the room also knows in which floor it is. 
+    /// In a room a set of plants can be placed. 
+
+    /// 
+    /// </summary>
+    public class Room
+    {
+        /// Constructor
+        /// <param name="roomName"></param>
+        public Room(string roomName) 
+        { 
+            RoomName = roomName;
+            FloorOfRoom = 0;
+        }
+
+        /// Constructor 2
+        /// <param name="roomName"></param>
+        /// <param name="floorofRoom"></param>
+        public Room(string roomName, int floorofRoom)
+        {
+            RoomName = roomName;
+            FloorOfRoom = floorofRoom;
+        }
+
+        public string RoomName { get; set; }
+        public int FloorOfRoom { get; set; }
+    }
+}
