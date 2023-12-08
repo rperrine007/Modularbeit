2205f636cee5f80440faa710c755e78abb77ac9d
Wüst Alexander (wuestal2) <wuestal2@students.zhaw.ch>
2023-12-08 16:58:39 +0100

    new project for testing

---
+++ PlantGeniusUser.GUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PlantGeniusUser.GUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
