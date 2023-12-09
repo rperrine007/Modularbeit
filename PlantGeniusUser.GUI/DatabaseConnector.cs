1757c54f77efd1622127264d4c917cfe3a4f979d
Wüst Alexander (wuestal2) <wuestal2@students.zhaw.ch>
2023-12-09 02:49:11 +0100

    init

--- PlantGeniusUser.GUI/DatabaseConnector.cs
+++ PlantGeniusUser.GUI/DatabaseConnector.cs
@@ -1,28 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 
 namespace PlantGeniusUser.GUI
 {
     public class DatabaseConnector
     {
-        public async Task<MySqlConnection> GetDatabaseConnectionAsync()
+        public async Task<List<Plant>> GetPlantsAsync()
         {
-            var builder = new MySqlConnectionStringBuilder
+            // Return static data for testing
+            return new List<Plant>
             {
-                Server = "49.12.196.20", // Server
-                Port = 14501,            // Port number
-                Database = "c1_zhaw2",     // Database
-                UserID = "c1_zhaw",      // User
-                Password = "lQ9fKVoNK7ll!" // Password
+                new Plant("Rose", DateTime.Now, 7),
+                new Plant("Tulpe", DateTime.Now.AddDays(-30), 5),
+                new Plant("Kaktus", DateTime.Now.AddDays(-60), 30),
             };
-
-            var connection = new MySqlConnection(builder.ConnectionString);
-            await connection.OpenAsync();
-            return connection;
         }
     }
 }
---
+++ PlantGeniusUser.GUI/Room.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantGeniusUser.GUI
+{
+    /// <summary>
+    /// A room has a name. If an apartment or a house has different floors, the room also knows in which floor it is. 
+    /// 
+    /// </summary>
+    public class Room
+    {
+        public Room()
+        {}
+
+        /// Constructor
+        /// <param name="roomName"></param>
+        /// <param name="floorOfRoom"></param>
+        /// <param name="roomLight"></param>
+        public Room(string roomName, int floorOfRoom, bool roomLight) 
+        {
+            RoomName = roomName;
+            FloorOfRoom = floorOfRoom;
+            RoomLight = roomLight;
+            RoomSortNumber = 0;
+        }
+
+
+        /// Constructor 2
+        /// <param name="roomID"></param>
+        /// <param name="roomName"></param>
+        /// <param name="roomSortNumber"></param>
+        /// <param name="floorOfRoom"></param>
+        /// <param name="roomLight"></param>
+        public Room(int roomID, string roomName, int roomSortNumber, int floorOfRoom, bool roomLight)
+        {
+            RoomID = roomID;
+            RoomName = roomName;
+            RoomSortNumber = roomSortNumber;
+            FloorOfRoom = floorOfRoom;
+            RoomLight = roomLight;
+        }
+
+        //Properties
+        public int RoomID { get; init; }
+        public string RoomName { get; set; }
+        public int RoomSortNumber {  get; set; }
+        public int FloorOfRoom { get; set; }
+        public bool RoomLight { get; set; }
+
+        //override ToString function
+        public override string ToString()
+        {
+            return $"RaumID: {RoomID}, Raumname: {RoomName}, Stockwerk: {FloorOfRoom}, viel Licht im Raum: {RoomLight}";
+        }
+
+    }
+}
