2ac2737d37543a5fab11585e63d2ad077c46cacd
Wüst Alexander (wuestal2) <wuestal2@students.zhaw.ch>
2023-12-11 17:03:26 +0100

    connection to db

--- PlantGeniusUser.GUI/DatabaseConnector.cs
+++ PlantGeniusUser.GUI/DatabaseConnector.cs
@@ -1,20 +1,57 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PlantGeniusUser.GUI
 {
-    public class DatabaseConnector
-    {
-        public async Task<List<Plant>> GetPlantsAsync()
-        {
-            // Return static data for testing
-            return new List<Plant>
-            {
-                new Plant("Rose", DateTime.Now, 7),
-                new Plant("Tulpe", DateTime.Now.AddDays(-30), 5),
-                new Plant("Kaktus", DateTime.Now.AddDays(-60), 30),
-            };
-        }
-    }
+	public class DatabaseConnector
+	{
+		private string connectionString = "server=49.12.196.20;port=14501;user=c1_zhaw;password=lQ9fKVoNK7ll!;database=c1_zhaw2";
+
+		public async Task<List<Plant>> GetPlantsAsync()
+		{
+
+			var plants = new List<Plant>();
+
+			//var builder = new MySqlConnectionStringBuilder
+			//{
+			//	Server = "49.12.196.20",		// Server
+			//	Port = 14501,					// Port number
+			//	Database = "c1_zhaw2",			// Database
+			//	UserID = "c1_zhaw",				// User
+			//	Password = "lQ9fKVoNK7ll!"		// Password
+			//};
+			using (var connection = new MySqlConnection(connectionString))
+			//using (var connection = new MySqlConnection(builder.ConnectionString))
+			{
+				await connection.OpenAsync();
+
+				using (var command = new MySqlCommand("SELECT * FROM Plant", connection))
+				using (var reader = await command.ExecuteReaderAsync())
+				{
+					while (await reader.ReadAsync())
+					{
+						var plant = new Plant(
+							reader["PlantName"].ToString(),
+							Convert.ToDateTime(reader["PlantWaterLastTime"]),
+							Convert.ToInt32(reader["PlantWaterRequirement"])
+						);
+						plants.Add(plant);
+					}
+				}
+			}
+
+			return plants;
+
+
+			//// Return static data for testing
+			//return new List<Plant>
+			//{
+			//    new Plant("Rose", DateTime.Now, 7),
+			//    new Plant("Tulpe", DateTime.Now.AddDays(-30), 5),
+			//    new Plant("Kaktus", DateTime.Now.AddDays(-60), 30),
+			//};
+		}
+	}
 }
