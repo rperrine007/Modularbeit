4bf8c1d9c9d7fce03768d9ab70fcd1c2a61cc9d3
Wüst Alexander (wuestal2) <wuestal2@students.zhaw.ch>
2023-12-19 21:55:03 +0100

    some other changes...

--- PlantGeniusUser.GUI/ViewModel/PlantViewModel.cs
+++ PlantGeniusUser.GUI/ViewModel/PlantViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Numerics;
 using System.Windows.Input;
+using Microsoft.EntityFrameworkCore;
+using PlantGenius.DAL;
 using PlantGenius.DAL.Models;
 
-namespace PlantGeniusUser.GUI.ViewModel 
+namespace PlantGeniusUser.GUI.ViewModel
 {
     public class PlantsViewModel : INotifyPropertyChanged
     {
@@ -17,6 +20,10 @@ namespace PlantGeniusUser.GUI.ViewModel
             Plants = new ObservableCollection<Plant>();
             EditCommand = new Command<Plant>(EditPlant);
             WaterCommand = new Command<Plant>(WaterPlant);
+
+            // Now get the plants from database
+            //LoadPlants();
+            LoadDemoData();
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -31,6 +38,37 @@ namespace PlantGeniusUser.GUI.ViewModel
             // 
         }
 
+        private void LoadPlants()
+        {
+            try
+            {
+                using (var dbContext = new AppDbContext())
+                {
+                    var plants = dbContext.Plants
+                        .Include(p => p.Room)
+                        .ToList();
+
+                    // Add items to the collection
+                    foreach (var plant in plants)
+                    {
+                        Plants.Add(plant);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+
+        }
+
+        private void LoadDemoData()
+        {
+            // Create and add demo plants
+            Plants.Add(new Plant("Rose", DateTime.Now, 7));
+            Plants.Add(new Plant("Kaktus", DateTime.Now, 5));
+            Plants.Add(new Plant("Testblume", DateTime.Now, 10));
+        }
+
     }
 }
-
