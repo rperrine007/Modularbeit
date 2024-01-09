7784fd9a274a4f7e196de146a9e2690413bc0ab8
Wüst Alexander (wuestal2) <wuestal2@students.zhaw.ch>
2024-01-10 00:56:47 +0100

    added german version neccessary for german  DatePicker

--- PlantGeniusUserApp.GUI/App.xaml.cs
+++ PlantGeniusUserApp.GUI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace PlantGeniusUserApp.GUI
+﻿using System.Globalization;
+
+namespace PlantGeniusUserApp.GUI
 {
     public partial class App : Application
     {
@@ -7,6 +9,11 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+
+            // Set the culture to German
+            CultureInfo ci = new CultureInfo("de-DE");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
         }
     }
 }
