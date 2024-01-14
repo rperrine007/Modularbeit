3b2dc44d0af5fb409980182549f687407cc1592f
Wüst Alexander (wuestal2) <wuestal2@students.zhaw.ch>
2024-01-14 13:07:42 +0100

    deleted some unused usings

--- PlantGeniusUserApp.GUI/ViewModel/MainViewModel.cs
+++ PlantGeniusUserApp.GUI/ViewModel/MainViewModel.cs
@@ -1,10 +1,4 @@
 ﻿using PlantGenius.DAL;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 
