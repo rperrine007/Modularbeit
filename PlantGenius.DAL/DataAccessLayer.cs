e405a282d573129664679a409f6ed0bb229b26af
Wüst Alexander (wuestal2) <wuestal2@students.zhaw.ch>
2024-01-16 21:59:09 +0100

    Added Reload function to LoadPlantsFromDB function to make sure the data is always acurate and not cached.

--- PlantGenius.DAL/DataAccessLayer.cs
+++ PlantGenius.DAL/DataAccessLayer.cs
@@ -225,6 +225,10 @@ namespace PlantGenius.DAL
 
                     foreach (var plant in plantsInRoom)
                     {
+                        // Force reload all plants from DB to ensure update really loads the newest data
+                        db.Entry(plant).Reload();
+
+                        // Add Plants to list
                         plantList.Add(plant);
                     }
 
